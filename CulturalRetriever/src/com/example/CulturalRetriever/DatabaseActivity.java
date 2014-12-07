c719177108a49a38065d39c52997da82fbd548bc
Matthew Landen <mlanden@umbc.edu>
2014-12-07 12:36:48 -0500

    stuff

--- CulturalRetriever/src/com/example/CulturalRetriever/DatabaseActivity.java
+++ CulturalRetriever/src/com/example/CulturalRetriever/DatabaseActivity.java
@@ -1,9 +1,26 @@
 package com.example.CulturalRetriever;
 
-import com.example.CulturalRetriever.R;
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class DatabaseActivity extends Activity {
 
@@ -11,77 +28,138 @@ public class DatabaseActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_database);
+		// setContentView(R.layout.activity_database);
+		// //
+		//String[] results = SQLRequest();
+		String[] row = { "entry1", "entry2", "entry3", "entry4", "entry5",
+				"entry6", "entry7" };
+		String[] column = { "Description", "Date Created", "Expiration Date",
+				"Photo URL" };
+		int rl = row.length;
+		int cl = column.length;
+
+		Log.d("--", "R-Lenght--" + rl + "   " + "C-Lenght--" + cl);
+		// enabled scroll for large data
+		ScrollView sv = new ScrollView(this);
+		TableLayout tableLayout = createTableLayout(row, column, rl, cl);
+		HorizontalScrollView hsv = new HorizontalScrollView(this);
+
+		hsv.addView(tableLayout);
+		sv.addView(hsv);
+		setContentView(sv);
+
+		// //
+		// SQLRequest();
 	}
 
-	
+	protected String[] SQLRequest() {
+		try {
+
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(
+					"http://userpages.umbc.edu/~mcpat1/331/androidSQLRequestPage.php");
+			client.execute(request);
+			HttpResponse response = client.execute(request);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+			//
+			StringBuffer sb = new StringBuffer("");
+			//String[] results;
+			String line = "";
+			while ((line = in.readLine()) != null) {
+				sb.append(line);
+				//break;
+			}
+			//String[] results = sb.toString();
+			//return results;
+
+			// //////////////////////////////
+		} catch (Exception e) {
+
+		}
+		return null;
+
+	}
+
+	public void makeCellEmpty(TableLayout tableLayout, int rowIndex,
+			int columnIndex) {
+		// get row from table with rowIndex
+		TableRow tableRow = (TableRow) tableLayout.getChildAt(rowIndex);
+
+		// get cell from row with columnIndex
+		TextView textView = (TextView) tableRow.getChildAt(columnIndex);
+
+		// make it black
+		textView.setBackgroundColor(Color.BLACK);
+	}
+
+	public void setHeaderTitle(TableLayout tableLayout, int rowIndex,
+			int columnIndex) {
+
+		// get row from table with rowIndex
+		TableRow tableRow = (TableRow) tableLayout.getChildAt(rowIndex);
+
+		// get cell from row with columnIndex
+		TextView textView = (TextView) tableRow.getChildAt(columnIndex);
+
+		textView.setText("Hello");
+	}
+
+	private TableLayout createTableLayout(String[] rv, String[] cv,
+			int rowCount, int columnCount) {
+		// 1) Create a tableLayout and its params
+		TableLayout.LayoutParams tableLayoutParams = new TableLayout.LayoutParams();
+		TableLayout tableLayout = new TableLayout(this);
+		tableLayout.setBackgroundColor(Color.BLACK);
+
+		// 2) create tableRow params
+		TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams();
+		tableRowParams.setMargins(1, 1, 1, 1);
+		tableRowParams.weight = 1;
+
+		for (int i = 0; i < rowCount; i++) {
+			// 3) create tableRow
+			TableRow tableRow = new TableRow(this);
+			tableRow.setBackgroundColor(Color.BLACK);
+
+			for (int j = 0; j < columnCount; j++) {
+				// 4) create textView
+				TextView textView = new TextView(this);
+				// textView.setText(String.valueOf(j));
+				textView.setBackgroundColor(Color.WHITE);
+				textView.setGravity(Gravity.CENTER);
+
+				String s1 = Integer.toString(i);
+				String s2 = Integer.toString(j);
+				String s3 = s1 + s2;
+				int id = Integer.parseInt(s3);
+				Log.d("TAG", "-___>" + id);
+				if (i == 0 && j == 0) {
+					textView.setText("0==0");
+				} else if (i == 0) {
+					Log.d("TAAG", "set Column Headers");
+					textView.setText(cv[j - 1]);
+				} else if (j == 0) {
+					Log.d("TAAG", "Set Row Headers");
+					textView.setText(rv[i - 1]);
+				} else {
+					textView.setText("" + id);
+					// check id=23
+					if (id == 23) {
+						textView.setText("ID=23");
+
+					}
+				}
+
+				// 5) add textView to tableRow
+				tableRow.addView(textView, tableRowParams);
+			}
+
+			// 6) add tableRow to tableLayout
+			tableLayout.addView(tableRow, tableLayoutParams);
+		}
+
+		return tableLayout;
+	}
 }
-/*
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import android.content.Context;
-import android.os.AsyncTask;
-import android.widget.TextView;
-
-public class SigninActivity  extends AsyncTask<String,Void,String>{
-
-   private TextView statusField,roleField;
-   private Context context;
-   private int byGetOrPost = 0; 
-   //flag 0 means get and 1 means post.(By default it is get.)
-   public SigninActivity(Context context,TextView statusField,
-   TextView roleField,int flag) {
-      this.context = context;;
-      byGetOrPost = flag;
-   }
-
-   protected void onPreExecute(){
-
-   }
-   @Override
-   protected String doInBackground(String... arg0) {
-
-         try{
-            String username = (String)arg0[0];
-            String password = (String)arg0[1];
-            String link="http://userpages.umbc.edu/~mcpat1/331/androidSQLpage.php";
-            String data  = URLEncoder.encode("username", "UTF-8") 
-            + "=" + URLEncoder.encode(username, "UTF-8");
-            data += "&" + URLEncoder.encode("password", "UTF-8") 
-            + "=" + URLEncoder.encode(password, "UTF-8");
-            URL url = new URL(link);
-            URLConnection conn = url.openConnection(); 
-            conn.setDoOutput(true); 
-            OutputStreamWriter wr = new OutputStreamWriter
-            (conn.getOutputStream()); 
-            wr.write( data ); 
-            wr.flush(); 
-            BufferedReader reader = new BufferedReader
-            (new InputStreamReader(conn.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            // Read Server Response
-            while((line = reader.readLine()) != null)
-            {
-               sb.append(line);
-               break;
-            }
-            return sb.toString();
-         }catch(Exception e){
-            return new String("Exception: " + e.getMessage());
-         }
-      }
-   }
-
-}*/
