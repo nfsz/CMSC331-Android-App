fc5a68fbc56def46cf7ecb2f60036704a10e2988
Matthew Landen <mlanden@umbc.edu>
2014-12-09 09:22:05 -0500

    replace go active update

--- CulturalRetriever/src/com/example/CulturalRetriever/GoActive.java
+++ CulturalRetriever/src/com/example/CulturalRetriever/GoActive.java
@@ -3,22 +3,20 @@ package com.example.CulturalRetriever;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import android.content.Intent;
+import android.content.Context;
 import android.location.Location;
-import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+
 public class GoActive extends FragmentActivity {
 	private GoogleMap map;
+	private LocationManager manager;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);    
@@ -27,17 +25,18 @@ public class GoActive extends FragmentActivity {
 	    map.setMyLocationEnabled(true);
 	    Timer beat = new Timer();
 	    TimerTask task = new MoveBeat();
-//	    beat.scheduleAtFixedRate(task, 5000, 10000);
+	    manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	    beat.scheduleAtFixedRate(task, 5000, 10000);
 		//map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 	}
 
 	public class MoveBeat extends TimerTask{
-		private Location old = map.getMyLocation();
+		private Location old;
+		
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			Location loc = map.getMyLocation();
-			
+			Location loc = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 			if(loc != null && old != null &&
 			(loc.getLatitude() != old.getLatitude() || loc.getLongitude() != old.getLongitude())){
 				map.addPolyline(new PolylineOptions().add(new LatLng(loc.getLatitude(),loc.getLongitude())
