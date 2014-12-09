da5719cf3e4659c9e84e26106f4f6a60a17c2c0b
Matthew Landen <mlanden@umbc.edu>
2014-12-08 22:31:22 -0500

    map

--- CulturalRetriever/src/com/example/CulturalRetriever/GoActive.java
+++ CulturalRetriever/src/com/example/CulturalRetriever/GoActive.java
@@ -17,8 +17,6 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-
-
 public class GoActive extends FragmentActivity {
 	private GoogleMap map;
 	@Override
@@ -29,69 +27,10 @@ public class GoActive extends FragmentActivity {
 	    map.setMyLocationEnabled(true);
 	    Timer beat = new Timer();
 	    TimerTask task = new MoveBeat();
-	    beat.scheduleAtFixedRate(task, 5000, 10000);
+//	    beat.scheduleAtFixedRate(task, 5000, 10000);
 		//map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.go_active_activity_actions, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		
-		// Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_search:
-                //openSearch(); //need to complete
-                return true;
-            case R.id.action_settings:
-                //openSettings(); //need to complete
-                return true;
-            case R.id.action_home:
-            	goHome();
-            	return true;
-            case R.id.action_go_active:
-            	//should be able to remove this!
-            	return true;
-            case R.id.action_landmark_it:
-            	goLandmarkIt();
-            	return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-	}
-	public void goHome(){
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
-	}
-	public void goLandmarkIt(){
-		Intent intent = new Intent(this, LandmarkIt.class);
-	    startActivity(intent);
-	}
-	 /**
-     * A placeholder fragment containing a simple view.
-     */
-    /*public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() { }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                  Bundle savedInstanceState) {
-              View rootView = inflater.inflate(R.layout.fragment_display_message,
-                      container, false);
-              return rootView;
-        }
-    }*/
-	
 	public class MoveBeat extends TimerTask{
 		private Location old = map.getMyLocation();
 		@Override
