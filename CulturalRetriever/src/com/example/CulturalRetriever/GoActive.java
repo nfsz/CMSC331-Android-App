5bd0339bfeaa1f62f3016e9429ad7da925c87235
Matthew Landen <mlanden@umbc.edu>
2014-12-09 14:04:52 -0500

    focus

--- CulturalRetriever/src/com/example/CulturalRetriever/GoActive.java
+++ CulturalRetriever/src/com/example/CulturalRetriever/GoActive.java
@@ -4,12 +4,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.SyncStateContract.Constants;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.appcompat.R.color;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -32,7 +34,7 @@ public class GoActive extends FragmentActivity implements LocationListener{
 	    Location start = getLocation();
 	    map.animateCamera(CameraUpdateFactory.newLatLngZoom
 	    		(new LatLng(start.getLatitude(), start.getLongitude()), 10));
-	//    beat.scheduleAtFixedRate(task, 5000, 10000);
+	    beat.scheduleAtFixedRate(task, 5000, 10000);
 		//map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 	}
 
@@ -47,7 +49,7 @@ public class GoActive extends FragmentActivity implements LocationListener{
 			(loc.getLatitude() != old.getLatitude() || loc.getLongitude() != old.getLongitude())){
 				map.addPolyline(new PolylineOptions().add(new LatLng(loc.getLatitude(),loc.getLongitude())
 					,new LatLng(old.getLatitude(), old.getLongitude())));
-			}
+			} 
 			if(loc != null){
 				old = loc;
 			}
